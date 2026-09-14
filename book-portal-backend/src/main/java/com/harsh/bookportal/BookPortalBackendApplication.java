9e558189145d358b501cca5a246c927ba77ed21b
Harsh Sahu <harshsahu0499@gmail.com>
2026-09-14 21:47:15 +0200

    Initial commit: Spring boot backend + react frontend

---
+++ book-portal-backend/src/main/java/com/harsh/bookportal/BookPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.bookportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPortalBackendApplication.class, args);
+	}
+
+}
